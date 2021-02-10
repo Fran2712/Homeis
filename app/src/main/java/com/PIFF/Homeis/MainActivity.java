21b70ef6509891ed895e619c5bf4c902e2bd7add
Francisco Ruiz <franruiz027@gmail.com>
2021-02-10 20:47:18 +0100

    Home screen creada

--- app/src/main/java/com/PIFF/Homeis/MainActivity.java
+++ app/src/main/java/com/PIFF/Homeis/MainActivity.java
@@ -1,8 +1,15 @@
 package com.PIFF.Homeis;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.bottomappbar.BottomAppBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        /*
+        BottomAppBar bottomAppBar = findViewById(R.id.bottom_app_bar);
+
+        //LISTENER BOTON CENTRAL
+        findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "FAB Clicked.", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //LISTENER ICONO DE LA IZQ
+        bottomAppBar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Menu clicked!", Toast.LENGTH_SHORT).show();
+//                sheetBehavior = BottomSheetBehavior.from(sheet);
+            }
+
+
+        });
+
+        //LISTENERS ICONOS DERECHA
+        bottomAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.bottom_app_bar_menu_bookmark:
+                        Toast.makeText(MainActivity.this, "Saves clicked.", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.bottom_app_bar_menu_search:
+                        Toast.makeText(MainActivity.this, "Search clicked.", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.bottom_app_bar_menu_share:
+                        Toast.makeText(MainActivity.this, "Share clicked.", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                return false;
+            }
+        });*/
+
     }
 }
