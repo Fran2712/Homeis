0180bd24a002fb1efb4cb80edd5f93c3f84cdab2
Ionutalin23 <ionutalin64@gmail.com>
2021-02-26 13:33:54 +0100

    Acceso Jsoup

--- app/src/main/java/com/PIFF/Homeis/Direccion.java
+++ app/src/main/java/com/PIFF/Homeis/Direccion.java
@@ -4,8 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.PIFF.Homeis.persistencia.AccesoJsoup;
+
+import java.util.HashMap;
+import java.util.List;
 
 public class Direccion extends AppCompatActivity {
     private Button btn_regster;
@@ -14,6 +20,8 @@ public class Direccion extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_direccion);
         btn_regster = findViewById(R.id.BTN_regster);
+        List<String> lista_paises= AccesoJsoup.obtenerCiudades();
+        Log.d("datos",lista_paises.toString());
         btn_regster.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
---
+++ app/src/main/java/com/PIFF/Homeis/persistencia/AccesoJsoup.java
@@ -0,0 +1,10 @@
+package com.PIFF.Homeis.persistencia;
+
+import java.util.List;
+
+public class AccesoJsoup {
+    public static List<String> obtenerCiudades() {
+        
+        return null;
+    }
+}
